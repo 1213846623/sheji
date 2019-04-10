3b7d8e24dcd14ee6031df431faedd0db5e405cd8
ChenJianHui <17782784539@163.com>
2019-04-10 10:23:13 +0800

    初始的

---
+++ src/components/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="index">
+		
+	</div>
+</template>
+
+<script>
+import { Icon } from 'vux'
+
+export default {
+  name: 'index',
+  components: {
+    Icon
+  },
+}
+</script>
+
+<style>
+</style>
